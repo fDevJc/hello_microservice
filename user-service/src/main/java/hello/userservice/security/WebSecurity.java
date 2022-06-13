d3b2c3c9f67d0e015ff85cffe284919686a344f0
fDevJc <fdevjc@gmail.com>
2022-06-13 16:15:50 +0900

    [refactor] WebSecurityConfigurerAdapter 제외하고 로그인처리

--- user-service/src/main/java/hello/userservice/security/WebSecurity.java
+++ user-service/src/main/java/hello/userservice/security/WebSecurity.java
@@ -4,12 +4,11 @@ import hello.userservice.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @RequiredArgsConstructor
@@ -18,21 +17,8 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     private final UserService userService;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http.csrf().disable();
-//        http.authorizeRequests().antMatchers("/users/**")
-//                .permitAll()
-////                .hasIpAddress("192.168.0.1")  //해당 아이피만 허용
-//                .and()
-//                .addFilter(getAuthenticationFilter())
-//        ;
-//        return http.build();
-//    }
-
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.authorizeRequests().antMatchers("/users/**")
                 .permitAll()
@@ -40,21 +26,21 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
                 .and()
                 .addFilter(getAuthenticationFilter())
         ;
+        return http.build();
     }
 
-    private AuthenticationFilter getAuthenticationFilter() throws Exception {
+    private AuthenticationFilter getAuthenticationFilter() {
+
         AuthenticationFilter authenticationFilter = new AuthenticationFilter();
-        authenticationFilter.setAuthenticationManager(authenticationManager());
+
+
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(bCryptPasswordEncoder);
+        provider.setUserDetailsService(userService);
+
+        // authenticationManager -> 기본적으로 ProviderManager
+        authenticationFilter.setAuthenticationManager(new ProviderManager(provider));
+
         return authenticationFilter;
     }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
-    }
-//    public UserDetailsManager userDetailsManager() {
-//        JdbcUserDetailsManager user = new JdbcUserDetailsManager();
-//        user.createUser();
-//        return null;
-//    }
 }
