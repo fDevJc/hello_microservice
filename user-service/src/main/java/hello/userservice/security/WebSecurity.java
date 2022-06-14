b1c6b63132be1c94a1b867a976ca5460817ec611
fDevJc <fdevjc@gmail.com>
2022-06-14 16:25:36 +0900

    apigateway-service config-service 연동

--- user-service/src/main/java/hello/userservice/security/WebSecurity.java
+++ user-service/src/main/java/hello/userservice/security/WebSecurity.java
@@ -21,6 +21,7 @@ public class WebSecurity {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable();
+        http.authorizeRequests().antMatchers("/actuator/**").permitAll();
         http.authorizeRequests().antMatchers("/users/**")
                 .permitAll()
 //                .hasIpAddress("192.168.0.1")  //해당 아이피만 허용
