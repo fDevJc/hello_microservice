235ea50c386fe6aa20e1ea1c770926e69a285101
fDevJc <fdevjc@gmail.com>
2022-06-16 11:55:21 +0900

    kafka, order-service mariadb연동

--- discovery-service/start.sh
+++ discovery-service/start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./gradlew build && java -jar ./build/libs/discovery-service-0.0.1-SNAPSHOT.jar
+./gradlew clean && ./gradlew build && java -jar ./build/libs/discovery-service-0.0.1-SNAPSHOT.jar
